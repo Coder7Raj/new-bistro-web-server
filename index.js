6de249bb6ba17169616419e4718387e466e55ed7
Coder7Raj <alwaysraj733@gmail.com>
2025-08-31 23:52:02 +0600

    create server successfully

---
+++ index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mdhkz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const db = client.db("NewBistroBoss");
+    const menuCollection = db.collection("menu");
+    app.get("/menu", async (req, res) => {
+      const menu = await menuCollection.find().toArray();
+      res.send(menu);
+    });
+
+    //
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
